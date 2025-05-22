768a1c9107a4b878a7a5acc08852191927647406
MoMousa-22 <MoMousa-22>
2025-05-22 07:48:38 +0300

    Fix Some imports related to UseCases.

--- lib/movies/domain/use_cases/get_popular_movies_use_case.dart
+++ lib/movies/domain/use_cases/get_popular_movies_use_case.dart
@@ -1,5 +1,5 @@
-import '../entities/movie.dart';
-import '../repository/base_movie_repository.dart';
+import 'package:movies_app/movies/domain/entities/movie.dart';
+import 'package:movies_app/movies/domain/repository/base_movie_repository.dart';
 
 class GetPopularMoviesUseCase {
   final BaseMovieRepository baseMovieRepository;
--- lib/movies/domain/use_cases/get_top_rated_movies_use_case.dart
+++ lib/movies/domain/use_cases/get_top_rated_movies_use_case.dart
@@ -1,5 +1,5 @@
-import '../entities/movie.dart';
-import '../repository/base_movie_repository.dart';
+import 'package:movies_app/movies/domain/entities/movie.dart';
+import 'package:movies_app/movies/domain/repository/base_movie_repository.dart';
 
 class GetTopRatedMoviesUseCase {
   final BaseMovieRepository baseMovieRepository;
